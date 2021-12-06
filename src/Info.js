1e7e166d59b0bec7d2f40c81d462def961b4f526
Jeff Clark <61337911+Jeff-Clark-Topeka@users.noreply.github.com>
2021-12-06 00:46:49 -0600

    fixed grammatical errors on info page

--- src/Info.js
+++ src/Info.js
@@ -5,22 +5,22 @@ function Info () {
         <div class="info-container">
             <div>
                 <h2>Code of Conduct</h2>
-                <p>(1) Respect all ING members- No religion or political discussions, etc...</p>
+                <p>(1) Respect all ING members. No religious or political discussions, etc.</p>
                 <p>(2) No cheating, hacking, glitching, or system tampering. Can get you kicked out of ING.</p>
-                <p>(3) No drama</p>
-                <p>(4) Discrimination of any type wil NOT be tolerated at all! Members will not discriminate against other members (regardless of rank,age etc.)</p>
-                <p>(5) All members must be 16 years or older with working mic.</p>
+                <p>(3) No drama.</p>
+                <p>(4) Discrimination of any type will NOT be tolerated at all! Members will not discriminate against other members, (regardless of rank, age, etc.)</p>
+                <p>(5) All members must be 16 years or older with a working mic/headset.</p>
                 <p>(6) Clan tag must be your rank.</p>
-                <p>(7) If called to party by a higher up you must respond ASAP.</p>
-                <p>(8) No unsportsman like conduct/Tea bagging is prohibited. Any form of in-game humiliation, will not be tolerated.</p>
+                <p>(7) If called to a party by a higher up you must respond ASAP.</p>
+                <p>(8) Unsportsman like conduct/tea bagging is prohibited. Any form of in-game humiliation, will not be tolerated.</p>
                 <p>(9) Harassment of any type will NOT be tolerated.</p>
                 <p>(10) No favoritism.</p>
-                <p>(11) All members in ING have a open door policy.(please follow chain of command)</p>
+                <p>(11) All members in ING have an open door policy. (Please follow chain of command.)</p>
                 <p>(12) You can only be in ING and no other clan or community.</p>
-                <p>(13) Never leave another ING member behind: No rage quitting.</p>
+                <p>(13) Never leave another ING member behind. (No rage quitting.)</p>
                 <p>(14) All ING members should attend at least two practices a week.</p>
                 <p>(15) Modded controllers are not allowed during practice and tournaments.</p>
-                <p>(16) Keep trash talking to a minimum, you can trash-talk just don't get to carried away.</p>
+                <p>(16) Keep trash talking to a minimum, you can trash-talk just don't get too carried away.</p>
                 <p>(17) Locations and Bios should be visible for everyone.</p>
                 <p>(18) HAVE A GOOD TIME!</p>
             </div>
@@ -28,30 +28,30 @@ function Info () {
                 <h2>How To Recruit</h2>
                 <p>(1) When a possible recruit is found they need to be age tested: Ask what year they were born in.</p>
                 <p>(2) A Recruit needs to have a fairly deep voice, must be mature, and 16 years old to join ING.</p>
-                <p>(3) The Recruiter needs to ask their Recruit, If they have ever been in a clan before, If they have then ask: What type of clan it was or is (If they are currectly in it), Report to the Squad Blue Suits, So it can travel through the Chain of Command. </p>
+                <p>(3) The Recruiter needs to ask their Recruit, If they have ever been in a clan before. If they have then ask what type of clan it was or is (If they are currectly in it) and report to the Squad Blue Suits, So it can travel through the Chain of Command. </p>
                 <p>(4) Then their Location needs to say "ING (Squad) (Division) RCT".</p>
-                <p>(5) The Recruit needs to add the Gen Tag of the squad they are being Recruited in.</p>
-                <p>(7) Then, The Recruiter must report to the General of their squad so the General can add them on the Gen Tag.</p>
-                <p>(8) Lastly, The Recruiter needs to go to there own bio to put "RCT: (#)" in between the date of their own Recruitment and the website, the RCT number must be updated everytime the Recruiter gets a new Recruit.</p>
+                <p>(5) The Recruit needs to add the Gen Tag of the squad they are being recruited in.</p>
+                <p>(7) Then, The Recruiter must report to the General of their squad so the General can add them to the Gen Tag.</p>
+                <p>(8) Lastly, The Recruiter needs to go to their own bio to put "RCT: (#)" in between the date of their own Recruitment and the website, the RCT number must be updated everytime the Recruiter gets a new Recruit.</p>
             </div>
             <div>
                 <h2>How To Squad Transfer</h2>
                 <p>(1) Report to the General of currect squad to request a Squad Transfer (Unless there is a problem with the General then the rank higher than the General will be reached).</p>
                 <p>(2) The Red Suits would then discuss it over and either approve or disapprove of the Squad Transfer.</p>
                 <p>(3) If approved by the Red Suits the General of the currect squad (or the next rank higher if there is a problem with the General) will go to the General of the Requested Squad to ask for Approval of Squad Transfer into their Squad.</p>
-                <p>(4) f the General of Requested Squad approves then, the one wanting to Squad Transfer would then bring their currect squad two recruits: One to replace them and One to make the squad stronger.</p>
+                <p>(4) If the General of Requested Squad approves then, the one wanting to Squad Transfer would then bring their currect squad two recruits: One to replace them and One to make the squad stronger.</p>
             </div>
             <div>
                 <h2>How To Division Transfer</h2>
-                <p>(1) Report to the General of currect squad to request a Division Transfer (Unless there is a problem with the General then the rank higher than the General will be reached).</p>
-                <p>(2) The Red Suits would then discuss it over and either approve of or disapprove of the Division Transfer.</p>
+                <p>(1) Report to the General of current squad to request a Division transfer (Unless there is a problem with the General then the rank higher than the General will be reached).</p>
+                <p>(2) The Red Suits would then discuss it over and either approve of or disapprove of the Division transfer.</p>
                 <p>(3) If approved by the Red Suits the President of the currect division will go to the General Manager for approval.</p>
                 <p>(4) If approved by the General Manager than the President of the currect division will go to the President of the requested divison where that President then will pick a squad and ask that General if they will accept the transfer.</p>
-                <p>(5) If the General in the Requested Division approves then,the one wanting to Division Transfer would then bring their currect squad two recruits: One to replace them and One to make the squad stronger.</p>
+                <p>(5) If the General in the Requested Division approves then,the one wanting to Division Transfer would then bring their current squad two recruits: One to replace them and One to make the squad stronger.</p>
             </div>
             <div>
                 <h2>How To Become A (Insert Rank)</h2>
-                <p>(RCT): Changed Location and Bio to required information. Added Clan Tag.</p>
+                <p>(RCT): Change Location and Bio to required information. Add Clan Tag.</p>
                 <p>(PVT): Sign-Up on Clan website</p>
                 <p>(CPL): Must get a TOTAL of two Co-Hosts, two Recruits, or one of each.</p>
                 <p>(SGT): Must get a TOTAL of four Co-Hosts, four Recruits, or any combination of four of both.</p>
